886ddcde3283333b106795ab51158c9072e6edf8
gedyra <gedyra@gmail.com>
2016-06-12 21:58:41 +0900

    update

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.vimrc ~/
-ln -sf ~/dotfiles/filetype.vim ~/
+ln -sf ~/dotfiles/filetype.vim ~/.vim
 ln -sf ~/dotfiles/vim/colors ~/.vim
 ln -sf ~/dotfiles/vim/ftplugin ~/.vim
